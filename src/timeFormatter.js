3d94781b231e3ba03e537d2aff57e2757b1566a4
Nadia <nadic206@yandex.ru>
2020-09-27 12:25:29 +0200

    feat: add task solution

--- src/timeFormatter.js
+++ src/timeFormatter.js
@@ -1,7 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function timeFornatter(/* sec */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function timeFornatter(sec) {
+  if(sec < 3600 && sec > 0) {
+    let min = Math.trunc(sec / 60);
+    sec = sec % 60;
+    min > 0 ? min = min + ' min' : min = '';
+    sec > 0 ? sec = sec + ' sec' : sec = '';
+    return `${min} ${sec}`.trim();
+} else {
+  throw new Error('wrong value')
+}
 };
 
